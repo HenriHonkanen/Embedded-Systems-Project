f15a2bdd3222862608d1436cfae70b34997b9b8e
Henri Honkanen <hshonkan@dragstrip.niksula.hut.fi>
2013-04-03 13:15:46 +0300

    Sensor filtering

--- carproject.c
+++ carproject.c
@@ -12,15 +12,119 @@
     DDRA = INPUT;\
     DDRD = OUTPUT\
 
+#define SMALL 10
+#define MEDIUM 20
+#define LARGE 40
 
 
 
 ISR(INT5_vect) {
-	SetMotorSpeed(50);
+	SetMotorSpeed(40);
+}
+
+float ShiftTowards( float target , float direction , float speed ) {
+
+	if((target-direction) > speed || (target-direction) < -speed ) {
+
+		if(direction < target) {
+			return direction+speed;
+		}
+		else if (direction > target) {
+			return direction-speed;
+		}
+	}
+	return target;
+}
+
+
+float sensors[8];
+
+void UpdateSensors() {
+
+	if (PINA < 0xff) {
+
+			if (!(PINA & _BV(PA7))) {
+				sensors[0]+=1.0f;
+			}
+			else {
+				sensors[0]-=0.1f;
+			}
+
+			if (!(PINA & _BV(PA6))) { 
+				sensors[1]+=1.0f;
+			}
+			else {
+				sensors[1]-=0.1f;
+			}
+
+			if (!(PINA & _BV(PA5))) { 
+				sensors[2]+=0.5f;
+			}
+			else {
+				sensors[2]-=0.1f;
+			}
+
+			if (!(PINA & _BV(PA4))) {
+				sensors[3]+=1.0f;
+			}
+			else {
+				sensors[3]-=0.1f;
+			}
+
+			if (!(PINA & _BV(PA3))) { 
+				sensors[4]+=1.0f;
+			}
+			else {
+				sensors[4]-=0.1f;
+			}
+
+			if (!(PINA & _BV(PA2))) {
+				sensors[5]+=1.0f;
+			}
+			else {
+				sensors[5]-=0.1f;
+			}
+
+			if (!(PINA & _BV(PA1))) { 
+				sensors[6]+=1.0f;
+			}
+			else {
+				sensors[6]-=0.1f;
+			}
+
+			if (!(PINA & _BV(PA0))) { 
+				sensors[7]+=1.0f;
+			}
+			else {
+				sensors[7]-=0.1f;
+			}
+
+		}
+
+		int i = 0;
+		for(i; i<8; i++) {
+			if(sensors[i] > 1.0f) {
+				sensors[i] = 1.0f;
+			}
+			else if(sensors[i] < 0.0f) {
+				sensors[i] = 0.0f;
+			}
+		}
+
+
+}
+
+bool GetFilteredSensorValue(int sensor) {
+	return (sensors[sensor]>0.2f);
 }
 
 int main(void)
 {  
+	int i = 0;
+	for(; i<8; i++) {
+			sensors[i] = 0;
+	}
+
 	sei();
 	SETUP_SENSOR_IO();
 
@@ -30,42 +134,101 @@ int main(void)
 	InitServo(0);
 	InitMotor();
 
+	float direction = 0;
+	int target = 0;
+	
+	int counter = 0;
+
 	for (;;)
-	{
+	{	
+		UpdateSensors();
+
+		int sensors_hit = 0;
+		//float shift = 0;
+
 		if (PINA < 0xff) {
-			ENABLE_COMPARE_OUTPUT_MODE(4);
-			//on = true;
-			//if (!(PINA & _BV(PA0))) {
-			if (!(PINA & _BV(PA7))) {
-				MoveServo(40);
+
+			counter=0;
+
+			if (GetFilteredSensorValue(0)) {
+				//shift += ShiftTowards(40,target,10);
+				target = LARGE;
+				sensors_hit++;
+				//MoveServo(40);
 			}
 
-			else if (!(PINA & _BV(PA6))) { 
-				MoveServo(30);
+			if (GetFilteredSensorValue(1)) { 
+				//shift += ShiftTowards(30,target,10);
+				target = MEDIUM;
+				sensors_hit++;
+				//MoveServo(30);
 			}
 
-			else if (!(PINA & _BV(PA5))) { 
-				MoveServo(20);
+			if (GetFilteredSensorValue(2)) { 
+				//shift += ShiftTowards(20,target,10);
+				target = SMALL;
+				sensors_hit++;
+				//MoveServo(20);
 			}
 
-			else if (!(PINA & _BV(PA4))) {
-				MoveServo(0);
+			if (GetFilteredSensorValue(3)) {
+				//shift += ShiftTowards(0,target,10);
+				target = 0;
+				sensors_hit++;
+				//MoveServo(0);
 			}
 
-			else if (!(PINA & _BV(PA3))) { 
-				MoveServo(0);
+			if (GetFilteredSensorValue(4)) { 
+				//shift += ShiftTowards(0,target,10);
+				target = 0;
+				sensors_hit++;
+				//MoveServo(0);
 			}
-			else if (!(PINA & _BV(PA2))) {
-				MoveServo(-20);
+			if (GetFilteredSensorValue(5)) {
+				//shift += ShiftTowards(-20,target,10);
+				target = -SMALL;
+				sensors_hit++;
+				//MoveServo(-20);
 			}
 
-			else if (!(PINA & _BV(PA1))) { 
-				MoveServo(-30);
+			if (GetFilteredSensorValue(6)) { 
+				//shift += ShiftTowards(-30,target,10);
+				target = -MEDIUM;
+				sensors_hit++;
+				//MoveServo(-30);
 			}
-			else if (!(PINA & _BV(PA0))) { 
-				MoveServo(-40);
+
+			if (GetFilteredSensorValue(7)) { 
+				//shift += ShiftTowards(-40,target,10);
+				target = -LARGE;
+				sensors_hit++;
+				//MoveServo(-40);
+			}
+
+			//target = shift/sensors_hit;
+
+			if(sensors_hit==1) {
+				MoveServo(target);
+
+				switch(target) {
+					case 0: SetMotorSpeed(70); break; 
+					case -SMALL:
+					case SMALL: SetMotorSpeed(50); break; 
+					case -MEDIUM:					
+					case MEDIUM: SetMotorSpeed(40); break;
+					case -LARGE: 
+					case LARGE: SetMotorSpeed(30); break; 
+				}
 			}
 		}
+		else {
+			counter++;
+
+			if(counter>3000) {
+				SetMotorSpeed(0);
+			}
+		}
+		//direction = ShiftTowards(target,direction,0.01);	
 
 	}
 return 0;
